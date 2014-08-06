7b238b414b5e39a0dba9533838dfcbcce8308912
akatiyar <akatiyar@csc.com>
2014-08-06 15:21:44 +0530

    Fixing the bug

--- web/src/main/webapp/grid.js
+++ web/src/main/webapp/grid.js
@@ -47,7 +47,7 @@ var iNettuts = {
                 $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
                     e.stopPropagation();
                 }).click(function () {
-                    if(confirm('HCL Training: This widget will be removed, ok?')) {
+                    if(confirm('This has been fixed: This widget will be removed, ok?')) {
                         $(this).parents(settings.widgetSelector).animate({
                             opacity: 0
                         },function () {
