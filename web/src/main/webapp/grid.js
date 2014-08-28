bf61a2625b0e59a9a484832fc03a0d82a2f1647e
akatiyar123 <ankur.katiyar@gmail.com>
2014-08-28 16:00:29 +0530

    demo-geetika

--- web/src/main/webapp/grid.js
+++ web/src/main/webapp/grid.js
@@ -47,7 +47,7 @@ var iNettuts = {
                 $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
                     e.stopPropagation();
                 }).click(function () {
-                    if(confirm('Latest: This widget will be removed, ok?')) {
+                    if(confirm('Geetika: This widget will be removed, ok?')) {
                         $(this).parents(settings.widgetSelector).animate({
                             opacity: 0
                         },function () {
