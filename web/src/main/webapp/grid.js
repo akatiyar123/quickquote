d9b73977e62675b41e06503bf2c610d4adf061a0
akatiyar123 <ankur.katiyar@gmail.com>
2014-09-11 16:46:32 +0530

    Final Commit

--- web/src/main/webapp/grid.js
+++ web/src/main/webapp/grid.js
@@ -47,7 +47,7 @@ var iNettuts = {
                 $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
                     e.stopPropagation();
                 }).click(function () {
-                    if(confirm('Deliberate Bug: This widget will be removed, ok?')) {
+                    if(confirm('Fixed Code: This widget will be removed, ok?')) {
                         $(this).parents(settings.widgetSelector).animate({
                             opacity: 0
                         },function () {
